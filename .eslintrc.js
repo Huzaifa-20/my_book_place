7b9cb44bf6eb45bb6b2525b70eaa62cf6cd439af
MoHuzaifa <muhammad.huzaifa@tintash.com>
2021-08-31 12:39:08 +0500

    Set up Husky pre-commit hook with ESLint,Prettier and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// Path: /.eslintrc.js
+
+module.exports = {
+  extends: ['react-app', 'airbnb'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    // You can override any rules you want
+  },
+};
