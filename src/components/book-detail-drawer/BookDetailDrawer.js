f6bf8adc20d1b633936e64fa1a714db6151fc844
MoHuzaifa <muhammad.huzaifa@tintash.com>
2021-09-02 01:19:54 +0500

    Integrate components to create homepage

--- src/components/book-detail-drawer/BookDetailDrawer.js
+++ src/components/book-detail-drawer/BookDetailDrawer.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaTimesCircle } from 'react-icons/fa';
 
 import './BookDetailDrawerStyle.scss';
 
-const BookDetailDrawer = () => (
-  <div className="outter-container">
-    <div className="drawer">
-      <h1 className="book-title">The Long Universe</h1>
-      <h3 className="book-genre">Sci-Fi</h3>
-      <h3 className="book-author">Terry Pratchett</h3>
-      <h3>All books by this author</h3>
-      <ul className="all-books">
-        <li>The Long Earth</li>
-        <li>The Color of Magic</li>
-        <li>The Light Fantastic</li>
-        <li>Hogfather</li>
-        <li>Thud</li>
-        <li>The Long Universe</li>
-      </ul>
+const BookDetailDrawer = ({ isDrawerOpen, closeDrawer }) => (isDrawerOpen ? (
+  <div className="drawer">
+    <div className="close-drawer">
+      <FaTimesCircle className="close-button" onClick={closeDrawer} />
     </div>
+
+    <h1 className="book-title">The Long Universe</h1>
+    <h3 className="book-genre">Sci-Fi</h3>
+    <h3 className="book-author">Terry Pratchett</h3>
+    <h3>All books by this author</h3>
+    <ul className="all-books">
+      <li>The Long Earth</li>
+      <li>The Color of Magic</li>
+      <li>The Light Fantastic</li>
+      <li>Hogfather</li>
+      <li>Thud</li>
+      <li>The Long Universe</li>
+    </ul>
   </div>
-);
+) : null);
 
 export default BookDetailDrawer;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 import BookDetailDrawer from '../../components/book-detail-drawer/BookDetailDrawer';
 import BookCard from '../../components/book-card/BookCard';
+import CustomButton from '../../components/custom-button/CustomButton';
+import './HomePageStyle.scss';
 
 const HomePage = () => {
-  const word = 'hello';
+  const [bookSelected, setBookSelected] = useState(false);
+
+  const bookSelectHandler = () => {
+    setBookSelected(!bookSelected);
+  };
+
+  const closeDrawer = () => {
+    setBookSelected(false);
+  };
+
+  const animation = useSpring(bookSelected ? { opacity: 1 } : { opacity: 0 });
+
   return (
-    <div>
-      {/* <BookDetailDrawer />
-      <BookCard /> */}
+    <div className="outter-container">
+      <div className="main-container">
+        <div className="upper-half">
+          <div className="main-heading">
+            <h1 className="user-title">Ninjas Reading List</h1>
+          </div>
+          <div className="books-container">
+            <div className="book-button">
+              <CustomButton selected={bookSelected} onClick={bookSelectHandler}>
+                The Long Earth
+              </CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>The Color of Magic</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>The Light Fantastic</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>The Final Empire</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>The Hero of Ages</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>The Name of the Wind</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>Hogfather</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>Thud</CustomButton>
+            </div>
+            <div className="book-button">
+              <CustomButton selected={false}>The Long Universe</CustomButton>
+            </div>
+          </div>
+        </div>
+        <BookCard />
+      </div>
+      <div className={` ${bookSelected ? 'show' : ''} book-drawer`}>
+        <BookDetailDrawer
+          isDrawerOpen={bookSelected}
+          closeDrawer={closeDrawer}
+        />
+      </div>
     </div>
   );
 };
