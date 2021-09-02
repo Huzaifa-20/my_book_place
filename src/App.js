9e4fa2c481365838d604df5df77eda2572e284de
MoHuzaifa <muhammad.huzaifa@tintash.com>
2021-09-02 15:49:15 +0500

    Add routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { auth } from './firebase/firebase.utils';
 
 import LogInSignUpPage from './pages/LogIn-SignUp/LogInSignUpPage';
 import HomePage from './pages/HomePage/HomePage';
@@ -7,8 +9,17 @@ import './App.css';
 function App() {
   return (
     <div>
-      <HomePage />
-      {/* <LogInSignUpPage /> */}
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route
+          path="/login-signup"
+          /* render={() => (currentUser ? <Redirect to="/" /> : <LogInSignUpPage />)} */
+        >
+          <LogInSignUpPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
